aedbb5805cca265e83dfedf5db4dcaf805b64701
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2024-03-28 15:15:23 +0100

    tweaks

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -33,6 +33,8 @@ return {
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-        require("none-ls.code_actions.eslint"),
+        require("none-ls.diagnostics.eslint_d"),
         null_ls.builtins.diagnostics.golangci_lint,
         null_ls.builtins.formatting.gofmt,
         null_ls.builtins.formatting.goimports,

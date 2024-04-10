72d459d5c0dfc93b79ee9522a313305f9773a9df
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2024-04-10 13:27:33 +0200

    update mappings

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,6 +3,7 @@ local options = {
     -- lua = { "stylua" },
     -- css = { "prettier" },
     -- html = { "prettier" },
+    go = {"gofumpt", "goimports", "golangci-lint",}
   },
 
   format_on_save = {
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>re1", "<cmd>NvimTreeResize 30<CR>", { desc = "Resize NvimTree" })
+map("n", "<leader>re2", "<cmd>NvimTreeResize 50<CR>", { desc = "Resize NvimTree" })
+map("n", "<leader>re3", "<cmd>NvimTreeResize 100<CR>", { desc = "Resize NvimTree" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<leader>pf", "<cmd>echo expand('%:p')<CR>", { desc = "Print full path" })

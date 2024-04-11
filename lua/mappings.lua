dbb8415b030c7ae5db115877d2f2093e173a493b
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2024-04-11 16:03:46 +0200

    formatting

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,3 +12,12 @@ map("n", "<leader>re2", "<cmd>NvimTreeResize 50<CR>", { desc = "Resize NvimTree"
 map("n", "<leader>re3", "<cmd>NvimTreeResize 100<CR>", { desc = "Resize NvimTree" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("n", "<leader>pf", "<cmd>echo expand('%:p')<CR>", { desc = "Print full path" })
+map("n", "<leader>bd", "<cmd>%bd|e#<CR>", { desc = "Delete All Other Buffers" })
+
+-- package info
+map("n", "<leader>ns", require("package-info").show, { desc = "Show Package Info" })
+map("n", "<leader>nc", require("package-info").hide, { desc = "Close Package Info" })
+map("n", "<leader>nt", require("package-info").toggle, { desc = "Toggle Package Info" })
+map("n", "<leader>ni", require("package-info").install, { desc = "Install Package" })
+map("n", "<leader>nu", require("package-info").update, { desc = "Update Package" })
+map("n", "<leader>np", require("package-info").change_version, { desc = "Change Package Version" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,29 +1,38 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = "BufWritePre", -- uncomment for format on save
-    config = function()
-      require("configs.conform")
-    end,
-  },
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require("configs.lspconfig")
-    end,
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("configs.copilot")
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
+		lazy = false,
+		event = "BufWritePre", -- uncomment for format on save
+		config = function()
+			require("configs.conform")
+		end,
+	},
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+	},
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("configs.copilot")
+		end,
+	},
+	{
+		"vuki656/package-info.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		config = function()
+			require("package-info").setup()
+		end,
+	},
 }

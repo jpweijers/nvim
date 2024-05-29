66435fb9431cf55140b7e2a059d40f402b880c55
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2024-05-29 10:43:35 +0200

    options

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,9 @@ require("nvchad.options")
 -- add yours here!
 local opt = vim.opt
 
+opt.cursorline = true
+opt.cursorlineopt = "both"
+
 opt.termguicolors = true
 opt.hidden = true
 opt.relativenumber = true

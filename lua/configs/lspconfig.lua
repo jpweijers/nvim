ee4988a6b3192e221da38568a7f867c0c9b42e4f
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2024-11-16 09:20:25 +0100

    configs

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
-local servers = { "html", "cssls", "ts_ls", "gopls", "lua_ls", "prismals", "pylsp" }
+local servers = { "html", "cssls", "tsserver", "ts_ls", "gopls", "lua_ls", "prismals", "pylsp" }
 
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
